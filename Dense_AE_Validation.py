253d5d44f4c68b06b3df2c6299e1dafb76b01c65
Tapojyoti Paul <tapojyotipaul@users.noreply.github.com>
2020-10-15 12:36:17 +0530

    Update Dense_AE_Validation.py

--- Dense_AE_Validation.py
+++ Dense_AE_Validation.py
@@ -610,6 +610,7 @@ if __name__=='__main__':
     ob_ct = 1  # Start with a single observation
     logging.info(STATS)
     temp_df = pd.DataFrame()
+    print("Inferencing Started")
     while ob_ct <= 10000:
         temp = run_inference(ob_ct)
         temp["No_of_Observation"] = ob_ct
